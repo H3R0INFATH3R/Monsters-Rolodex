f18de40c3025964d0956524e852187b13cfe0800
Don <dagem.alemseged1@gmail.com>
2023-03-17 13:13:07 +0300

    First version of Monsters Rolodex

---
+++ src/App.js
@@ -0,0 +1,64 @@
+import { Component } from 'react'
+import CardList from './components/card-list/card-list.component'
+import SearchBox from './components/search-box/search-box.component'
+import './App.css';
+
+class App extends Component {
+  constructor() {
+    super()
+
+    this.state = {
+      monsters: [],
+      searchField: ''
+    }
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(users =>
+        this.setState(
+          () => {
+            return { monsters: users }
+          }
+        )
+      )
+  }
+
+  onSearchChange = event => {
+    const searchField = event.target.value.toLowerCase()
+    this.setState(
+      () => {
+        return { searchField }
+      }
+    )
+  }
+  
+  render() {
+
+    const { searchField, monsters } = this.state
+    const { onSearchChange } = this
+
+
+    const filteredMonsters = monsters.filter(monster => {
+      return monster.name.toLocaleLowerCase().includes(searchField)
+    })
+
+    return (
+      <div className="App">
+        <h1 className='app-title'>Monsters Rolodex</h1>
+
+        { <SearchBox 
+          onChangeHandler = { onSearchChange } 
+          className = 'monsters-search-box' 
+          placeholder = 'Search Monsters' /> 
+        }
+        { <CardList 
+          monsters = { filteredMonsters } /> 
+        }
+      </div>
+    )
+  }
+}
+
+export default App;
